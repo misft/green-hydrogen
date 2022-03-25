cb2f37703c151c49c86c97f228cb0c97b7e785fd
misft <indrawandwiprasetyo@gmail.com>
2022-03-25 15:00:33 +0700

    [update]

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description"
-        content="Poco admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords"
-        content="admin template, Poco admin template, dashboard template, flat admin template, responsive admin template, web app (Laravel 8)">
+        content=""
+        content="">
     <meta name="author" content="pixelstrap">
     <link rel="icon" href="{{ route('/') }}/assets/images/favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="{{ route('/') }}/assets/images/favicon.png" type="image/x-icon">
